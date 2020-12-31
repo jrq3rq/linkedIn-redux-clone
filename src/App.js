da591e1161fd06d5109927ad784dfa8cfaab8e6a
YOUR NAME <YOUR EMAIL ADDRESS>
2020-12-31 14:26:03 -0800

    Login component started

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 // import Footer from "./components/Footer/Footer";
 // import Widgets from "./components/Widgets/Widgets";
 import Feed from "./components/Feed/Feed";
+import { selectUser } from "./features/userSlice";
+import Login from "./components/Login/Login";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
       <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+        </div>
+      )}
     </div>
   );
 }
