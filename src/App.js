535f8d4dff462a9843eeb824f1423442bc29253d
YOUR NAME <YOUR EMAIL ADDRESS>
2020-12-30 16:49:51 -0800

    input option added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 // import Footer from "./components/Footer/Footer";
 // import Widgets from "./components/Widgets/Widgets";
-// import Feed from "./components/Feed/Feed";
+import Feed from "./components/Feed/Feed";
 
 function App() {
   return (
@@ -12,16 +12,8 @@ function App() {
       <Header />
       <div className="app__body">
         <Sidebar />
+        <Feed />
       </div>
-      {/* <Sidebar />
-      <Feed />
-      <Widgets />
-      <Footer /> */}
-      {/* Header */}
-      {/* App body */}
-      {/* sidebar */}
-      {/* feed */}
-      {/* widgets */}
     </div>
   );
 }
--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,10 +1,39 @@
 import React from "react";
+import "./FeedElements.css";
+import CreateIcon from "@material-ui/icons/Create";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import InputOption from "../Input/InputOption";
 
 const Feed = () => {
   return (
-    <>
-      <h1>Header Element</h1>
-    </>
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form action="">
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="photo" color="#70b5f9" />
+          <InputOption
+            Icon={SubscriptionsIcon}
+            title="subscription"
+            color="#E7A33F"
+          />
+          <InputOption Icon={EventNoteIcon} title="event" color="#A0B4B7" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="calendar"
+            color="#F5987E"
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
